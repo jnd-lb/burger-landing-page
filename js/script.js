9e05f813d3bd9a9ff3ac6e66a617ac425c1910f9
jihad.noureddine <jihad.noureddine@tedmob.com>
2022-06-20 07:36:54 +0300

    updates

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+let data = [];
+
+let text =  $("#testimonial-text");
+let name = $("#testimonial-author");
+let title = $("#testimonial-title");
+
+//data 
+  $.getJSON('/js/data.json', function(_data) {
+    //TODO handle errors
+    data = _data;
+    updateTestimonial(0);
+  });
+
+  const updateTestimonial = (index)=>{
+      setTimeout(function(){
+          $(".testimonial-text").fadeOut(function(){
+              text.text(data[index].text)
+              name.text(data[index].name)
+              title.text(data[index].title)
+              updateTestimonial((index+1)%data.length);
+          });
+        }, 4000);
+        $(".testimonial-text").fadeIn();
+  }
+
+  let navBar = $("nav");
+
+  $(window).scroll(function() {
+    let scroll =  $(window).scrollTop()
+    if(scroll > 10 && !navBar.hasClass("nav-floating") ){
+        navBar.addClass("nav-floating")
+    }
+
+    if(scroll < 10){
+      navBar.removeClass("nav-floating")
+    }
+  })
+
+
+  //Slick Initialise
+  $('.your-class').slick({autoplay:true});
+
+
+
+  //Navbar Change active link
+  $('a').click(function(){
+    let hash = $(this).attr('href');
+    $('.active').removeClass('active');
+    $("a[href='"+hash+"']").addClass('active');
+  })
+})
